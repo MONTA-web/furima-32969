20dc6439db5084f4f2d751c5f9d3fbb75d5f2c71
MONTA-web <montamonta22@icoud.com>
2021-01-03 22:45:22 +0900

    単体テストコードの実装

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :item do
-    
+    product_name {"オムライス"}
+    description {Faker::Lorem.sentence}
+    category_id { 1 }
+    condition_id { 1 }
+    shipping_charge_id { 1 }
+    prefecture_id { 1 }
+    deadline_id { 1 }
+    price { 77000 }
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/tedy_image.jpg'),filename: 'tedy_image.jpg')
+    end
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,91 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '商品出品機能' do
+    before do
+      @item = FactoryBot.build(:item)
+    end
+
+    it "imageが空だと保存することができない" do
+      @item.image = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Image can't be blank")
+    end
+
+    it "product_nameが空だと保存することができない" do
+      @item.product_name = ""
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Product name can't be blank")
+    end
+
+    it "descriptionが空だと保存することができない" do
+      @item.description=""
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Description can't be blank")
+    end
+
+    it "categoryが選択されていないと保存することができない" do 
+      @item.category_id = 0
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Category Select")
+    end
+
+    it "conditionが選択されていないと保存することができない" do
+      @item.condition_id = 0
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Condition Select")
+    end
+
+    it "shipping_chargeが選択されていないと保存することができない" do
+      @item.shipping_charge_id = 0
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Shipping charge Select")
+    end
+
+    it "prefectureが選択されていないと保存することができない" do
+      @item.prefecture_id = 0
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Prefecture Select")
+    end
+
+    it "deadlineが選択されていないと保存することができない" do
+      @item.deadline_id = 0
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Deadline Select")
+    end
+
+    it "priceが空だと保存することができない" do
+      @item.price = ""
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price can't be blank",
+        "Price Half-width number",
+        "Price Out of setting range")
+    end
+
+    it "priceは300より低いと保存できない" do
+      @item.price = 299
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price Out of setting range")
+    end
+
+    it "priceは9,999,999より大きいと保存することができない" do
+      @item.price = 10000000
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price Out of setting range")
+    end
+
+    it "priceは半角数字しか保存することができない" do
+      @item.price = "１００００"
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price Half-width number", "Price Out of setting range")
+    end
+
+    it "全ての情報を適切に入力すると保存することができる" do
+      @item.save
+      expect(@item).to be_valid
+    end
+
+
+
+  end
 end
