1d62b6575bfb5721f0d0ca2b229faa47c68e57cf
MONTA-web <montamonta22@icoud.com>
2021-01-06 17:41:38 +0900

    商品購入データの保存する機能

--- app/models/item_order.rb
+++ app/models/item_order.rb
@@ -1,7 +1,7 @@
 class ItemOrder
 
   include ActiveModel::Model
-  attr_accessor :postal_code,:prefecture_id,:city,:house_number,:building_name,:phone
+  attr_accessor :postal_code,:prefecture_id,:city,:house_number,:building_name,:phone,:user_id,:item_id
 
   with_options presence: true do
     validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/}
@@ -12,7 +12,7 @@ class ItemOrder
     validates :prefecture_id, numericality:{ other_than: 0, message: "Select"}
 
   def save
-    Order.create(user_id: user.id, item_id: item.id)
+    order = Order.create(user_id: user_id,item_id: item_id)
     Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, house_number: house_number,building_name: building_name, phone: phone, order_id: order.id)
   end
 
